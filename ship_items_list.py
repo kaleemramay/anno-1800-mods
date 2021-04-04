95f1939274b27d70c90084180dc3ad473cb4756a
kaleemramay <33531984+kaleemramay@users.noreply.github.com>
2021-04-04 05:50:00 -0400

    initial commit

---
+++ ship_items_list.py
@@ -0,0 +1,497 @@
+shipSpecialist = [
+    {
+    "guid" : 191444,
+    "name" : "Sailor" 
+    },{
+    "guid" : 191445,
+    "name" : "Mate" 
+    },{
+    "guid" : 191446,
+    "name" : "Midshipman" 
+    },{
+    "guid" : 191447,
+    "name" : "Bo\'sun\'" 
+    },{
+    "guid" : 191448,
+    "name" : "Tugboatman" 
+    },{
+    "guid" : 191449,
+    "name" : "Pilot" 
+    },{
+    "guid" : 190766,
+    "name" : "Captain Ibrahim"
+    },{
+    "guid" : 191450,
+    "name" : "Captain Kadijah" 
+    },{
+    "guid" : 191451,
+    "name" : "Captain Felicity Phoenix, Back From the Grave"
+    },{
+    "guid" : 191452,
+    "name" : "Captain Richard Moses, Skipper of Schooners" 
+    },{
+    "guid" : 191453,
+    "name" : "Captain Smith, Titan of the Sea"
+    },{
+    "guid" : 191596,
+    "name" : "Ensign" 
+    },{
+    "guid" : 191597,
+    "name" : "Gunsmith" 
+    },{
+    "guid" : 191598,
+    "name" : "Former Pirate" 
+    },{
+    "guid" : 191599,
+    "name" : "Sergeant" 
+    },{
+    "guid" : 191600,
+    "name" : "Captain of Marines" 
+    },{
+    "guid" : 191601,
+    "name" : "Former Corsair"
+    },{
+    "guid" : 191602,
+    "name" : "Ship's Chaplain"
+    },{
+    "guid" : 191603,
+    "name" : "Vice-Admiral Adams"
+    },{
+    "guid" : 191604,
+    "name" : "Vice-Admiral Romanov"
+    },{
+    "guid" : 191605,
+    "name" : "Vice-Almirante Gonçalves"
+    },{
+    "guid" : 191606,
+    "name" : "Vice-Almirante Paulista"
+    },{
+    "guid" : 191607,
+    "name" : "Wilma the Wild, Bearer of Barracudas"
+    },{
+    "guid" : 191608,
+    "name" : "Lord High Admiral Nadasky, The Astute"
+    },{
+    "guid" : 191609,
+    "name" : "Georgina Duffy, The Call your Bluffy"
+    },{
+    "guid" : 191610,
+    "name" : "Admiral Haynes, Beholder of Far Horizons"
+    },{
+    "guid" : 191611,
+    "name" : "Commmodore Stephen Drake, The Besieger"
+    },{
+    "guid" : 191615,
+    "name" : "Lader"
+    },{
+    "guid" : 191616,
+    "name" : "Handler" 
+    },{
+    "guid" : 191617,
+    "name" : "Merchant" 
+    },{
+    "guid" : 191618,
+    "name" : "Shipper" 
+    },{
+    "guid" : 191619,
+    "name" : "Port Dealer"
+    },{
+    "guid" : 191620,
+    "name" : "Exporter of Goods"
+    },{
+    "guid" : 191621,
+    "name" : "Itsuko the International Exporter"
+    },{
+    "guid" : 191622,
+    "name" : "Salima the Shipping Operator"
+    },{
+    "guid" : 191623,
+    "name" : "Delores the Diplomat"
+    },{
+    "guid" : 191624,
+    "name" : "Ermenegilda Di Mercante, Purveyor of Tall Ships"
+    },{
+    "guid" : 191625,
+    "name" : "Lalla Mazigh, Merchant of a Free Market"
+    },{
+    "guid" : 191626,
+    "name" : "Hans Eichendorf, A Wholesale Success"
+    },{
+    "guid" : 192444,
+    "name" : "Drew Durnil the Speculative Historian"
+    },{
+    "guid" : 192449,
+    "name" : "Lady Lu the Redeemed Pirate"
+    },{
+    "guid" : 193188,
+    "name" : "Drae von Gast"
+    },{
+    "guid" : 192074,
+    "name" : "Jack of all Traits"
+    },{
+    "guid" : 190105,
+    "name" : "Dr Snuggles"
+    },{
+    "guid" : 190800,
+    "name" : "Ship Captain"
+    },{
+    "guid" : 192093,
+    "name" : "Pirate Cadet"
+    },{
+    "guid" : 192092,
+    "name" : "Willma von Tegetthoff"
+    },{
+    "guid" : 192090,
+    "name" : "Captain Moby, Old Dog of the Sea"
+    },{
+    "guid" : 192091,
+    "name" : "Geordy Duff"
+    },{
+    "guid" : 111126,
+    "name" : "Incendiary"
+    },{
+    "guid" : 111127,
+    "name" : "Arsonist"
+    },{
+    "guid" : 111128,
+    "name" : "Jittery Pyromaniac"
+    },{
+    "guid" : 111129,
+    "name" : "Wild Anarchist Vacco"
+    },{
+    "guid" : 111131,
+    "name" : "Wild Anarchist Sacchetti"
+    },{
+    "guid" : 111130,
+    "name" : "Ella Golden, The Anarchist Luminary"
+    }
+]
+
+vehicleItems = [
+    {
+    "guid" : 191824,    
+    "name" : "Square Rig"
+    },{
+    "guid" : 191013,
+    "name" : "Lateen Style Sails" 
+    },{
+    "guid" : 191825,
+    "name" : "Fore-and-aft Rig Sails" 
+    },{
+    "guid" : 191829,
+    "name" : "Two-turn Screw Propeller" 
+    },{
+    "guid" : 191830,
+    "name" : "One-turn Screw Propeller" 
+    },{
+    "guid" : 191831,
+    "name" : "Controllable-pitch Propeller" 
+    },{
+    "guid" : 191838,
+    "name" : "Fine Spyglass" 
+    },{
+    "guid" : 191839,
+    "name" : "High-Powered Scope" 
+    },{
+    "guid" : 191840,
+    "name" : "Telemobiloscope" 
+    },{
+    "guid" : 191848,
+    "name" : "Fine Wood Hull" 
+    },{
+    "guid" : 191849,
+    "name" : "Fine Steel Hull" 
+    },{
+    "guid" : 191850,
+    "name" : "Reinforced Bulkhead" 
+    },{
+    "guid" : 191851,
+    "name" : "Singh's Steel Double-Hull" 
+    },{
+    "guid" : 191852,
+    "name" : "Congrave Rocket" 
+    },{
+    "guid" : 191853,
+    "name" : "18-pounder Long Gun" 
+    },{
+    "guid" : 191854,
+    "name" : "Hans Gun" 
+    },{
+    "guid" : 191855,
+    "name" : "Strongarm's Disappearing Gun" 
+    },{
+    "guid" : 191856,
+    "name" : "Exhaust Blower" 
+    },{
+    "guid" : 191857,
+    "name" : "Heat Exchanger" 
+    },{
+    "guid" : 191858,
+    "name" : "Plate Heat Exchanger" 
+    },{
+    "guid" : 191878,
+    "name" : "White Flag" 
+    },{
+    "guid" : 191991,
+    "name" : "Great White Flag" 
+    },{
+    "guid" : 190161,
+    "name" : "Test Quest Item" 
+    },{
+    "guid" : 190024,
+    "name" : "Crappy Sails" 
+    },{
+    "guid" : 190025,
+    "name" : "Crappy Steam Motor" 
+    },{
+    "guid" : 6000004,
+    "name" : "SimpleTestItem" 
+    },{
+    "guid" : 190138,
+    "name" : "one time equip Sails" 
+    },{
+    "guid" : 190591,
+    "name" : "Ship Mechanic" 
+    },{
+    "guid" : 190592,
+    "name" : "Spyglass" 
+    },{
+    "guid" : 193397,
+    "name" : "Depressuriser" 
+    },{
+    "guid" : 192086,
+    "name" : "Basic Cannon Upgrade" 
+    },{
+    "guid" : 192087,
+    "name" : "Cannon Upgrade" 
+    },{
+    "guid" : 192088,
+    "name" : "Advanced Cannons" 
+    },{
+    "guid" : 192089,
+    "name" : "State-of-the-Art Cannons" 
+    },{
+    "guid" : 192112,
+    "name" : "Sail Ship Annihilator" 
+    },{
+    "guid" : 192113,
+    "name" : "Steam Ship Annihilator" 
+    },{
+    "guid" : 192114,
+    "name" : "Building Annihilator" 
+    },{
+    "guid" : 112548,
+    "name" : "Ballasted Diving Bell" 
+    },{
+    "guid" : 112549,
+    "name" : "Moon Pool Diving Bell" 
+    },{
+    "guid" : 112550,
+    "name" : "\"Spoondrift\" — Nathaniel's Pressurised Diving Bell"
+    },{
+    "guid" : 112554,
+    "name" : " Ballast Cable" 
+    },{
+    "guid" : 112555,
+    "name" : "Caoutchouc Cable" 
+    },{
+    "guid" : 112556,
+    "name" : "Advanced Umbilical Cable"
+    },{
+    "guid" : 112368,
+    "name" : "IndiscrimiN8r" 
+    },{
+    "guid" : 112369,
+    "name" : "ExtermiN8r" 
+    },{
+    "guid" : 112370,
+    "name" : "DomiN8r"
+    },{
+    "guid" : 112377,
+    "name" : "Jet Propeller— \"Squall\"" 
+    },{
+    "guid" : 112378,
+    "name" : "Jet Propeller— \"Cyclone\"" 
+    },{
+    "guid" : 112379,
+    "name" : "Jet Propeller— \"Hurricane\""
+    },{
+    "guid" : 113353,
+    "name" : "Renowned Excavator" 
+    },{
+    "guid" : 113354,
+    "name" : "Audrey the Marine Archaeologist" 
+    },{
+    "guid" : 113355,
+    "name" : "Peter Flinders, Conservator of Artefacts" 
+    },{
+    "guid" : 113356,
+    "name" : "Marine Biologist" 
+    },{
+    "guid" : 113357,
+    "name" : "Scara the Marine Biologist" 
+    },{
+    "guid" : 113358,
+    "name" : "Orvette Charles, Of The Royal Society of Oceanographers." 
+    },{
+    "guid" : 113359,
+    "name" : "Hobby Diver" 
+    },{
+    "guid" : 113360,
+    "name" : "Nadia the Spotter" 
+    },{
+    "guid" : 113361,
+    "name" : "Anthony Anchovy, Salvager Of Sunken Sumps" 
+    },{
+    "guid" : 113362,
+    "name" : "Treasure Seeker" 
+    },{
+    "guid" : 113363,
+    "name" : "Lamie The Wistful Pirate " 
+    },{
+    "guid" : 113364,
+    "name" : "Mai Kalan, Descryer Of Brilliant Jewels" 
+    }
+]
+
+vehicleBuffs = [
+    {
+    'guid' : 191595,
+    "name" : "Captain Ibrahim\'s Shield" 
+    },{
+    'guid' : 191835,
+    "name" : "Ship Turbine" 
+    },{
+    'guid' : 191836,
+    "name" : "Ship Piston Engine" 
+    },{
+    'guid' : 191837,
+    "name" : "Multiple Expansion Steam Engine" 
+    },{
+    'guid' : 191859,
+    "name" : "Mounted gun" 
+    },{
+    'guid' : 191861,
+    "name" : "Flame Hose" 
+    },{
+    'guid' : 191870,
+    "name" : "Flame Thrower" 
+    },{
+    'guid' : 191871,
+    "name" : "Pyrphorian Flame Thrower" 
+    },{
+    'guid' : 191943,
+    "name" : "Forged Steel Shells" 
+    },{
+    'guid' : 191944,
+    "name" : "Cemented Steel Shells" 
+    },{
+    'guid' : 191947,
+    "name" : "Melinite Shells" 
+    },{
+    'guid' : 191948,
+    "name" : "Dunnite Shells" 
+    },{
+    'guid' : 191949,
+    "name" : "Canister Shot" 
+    },{
+    'guid' : 191950,
+    "name" : "Double Canister Shot" 
+    },{
+    "guid" : 191981,
+    "name" : "Ship Toolbox" 
+    },{
+    'guid' : 192156,
+    "name" : "Ship Tools" 
+    },{
+    'guid' : 191983,
+    "name" : "Ship Tool Trolley" 
+    },{
+    'guid' : 192157,
+    "name" : "Modern Ship Tools" 
+    },{
+    'guid' : 191988,
+    "name" : "White Flag" 
+    },{
+    'guid' : 191994,
+    "name" : "Great White Flag" 
+    },{
+    'guid' : 192009,
+    "name" : "Trading Charter" 
+    },{
+    'guid' : 192010,
+    "name" : "Grand Trading Charter" 
+    },{
+    'guid' : 192011,
+    "name" : "Royal Trading Charter" 
+    },{
+    'guid' : 192174,
+    "name" : "Black Flag" 
+    },{
+    'guid' : 192175,
+    "name" : "Pirate Captain\'s Black Flag" 
+    },{
+    'guid' : 192453,
+    "name" : "Chain Shots" 
+    },{
+    'guid' : 192454,
+    "name" : "Bar Shots" 
+    },{
+    'guid' : 192940,
+    "name" : "Letter of Marque" 
+    },{
+    'guid' : 192941,
+    "name" : "Royal Letter of Marque" 
+    },{
+    'guid' : 192942,
+    "name" : "Letter of Immunity" 
+    },{
+    'guid' : 192943,
+    "name" : "Royal Letter of Immunity" 
+    },{
+    'guid' : 193240,
+    "name" : "Torpedo" 
+    },{
+    'guid' : 193241,
+    "name" : "Howitzer" 
+    },{
+    'guid' : 193242,
+    "name" : "Mortar" 
+    },{
+    'guid' : 112200,
+    "name" : "Torpedo" 
+    },{
+    'guid' : 112924,
+    "name" : "Naval Mine" 
+    }
+]
+
+
+def modparser(guid, name):
+    textparsed = """
+    <!-- {name} -->
+    <ModOp Type=\"add\" GUID=\'{guid}\' Path=\"/Values\">
+        <ItemEffect>
+            <EffectTargets>
+                <Item>
+                    <GUID>191458</GUID>
+                </Item>
+            </EffectTargets>
+        </ItemEffect>
+    </ModOp>      
+    """.format(name=name, guid=guid)
+    return textparsed
+
+
+print("<!-- Ship Specialists -->")
+for specialist in shipSpecialist:
+    print(modparser(specialist["guid"], specialist["name"]))
+
+print("<!-- Vehicle Items -->")
+for item in vehicleItems:
+    print(modparser(item["guid"], item["name"]))
+
+print("<!-- Vehicle Buffs -->")
+for buff in vehicleBuffs:
+    print(modparser(buff["guid"], buff["name"]))
+
